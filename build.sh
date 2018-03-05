a9fd0744b85dec5d9b1e577b10b4905f0100dd68
Bestfast <31173611+Bestfast@users.noreply.github.com>
2018-03-06 00:40:15 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ sudo curl http://www.sqlite.org/2018/sqlite-amalgamation-3220000.zip --output /u
 sudo unzip /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/sqlite.zip -d /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/
 sudo mv /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/sqlite-amalgamation-3220000/* /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/
 sudo mkdir $PWD/sqlite3
-sudo cp /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/sqlite3* $PWD/sqlite3/*
-sudo cp /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/shell.c $PWD/sqlite3/*
+sudo cp /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/sqlite3* $PWD/sqlite3/
+sudo cp /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/shell.c $PWD/sqlite3/
 g++ main.cpp -std=c++11 -lsqlite3 -o Generatore_Schede
 sed -i 's/<sqlite3.h>/"sqlite3.h"/g' main.cpp
 sudo x86_64-w64-mingw32-g++ -std=gnu++11 main.cpp -Isqlite3 -o Generatore_Schede.exe 
