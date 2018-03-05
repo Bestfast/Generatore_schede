92f12f6e4d8b8f4add95fe54305eba2a7a567fcd
Bestfast <31173611+Bestfast@users.noreply.github.com>
2018-03-05 15:42:01 +0100

    g++ rompe. Messa la flag per l'uso di std-c++11

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 sudo apt update 
 sudo apt install  -y gcc sqlite3 libsqlite3-dev build-essential
-g++ main.cpp -lsqlite3 -o generatore_schede
+g++ main.cpp -std=c++11 -lsqlite3 -o generatore_schede
