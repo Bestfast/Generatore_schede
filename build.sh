293b7afbe1f4b19642d60c32a8683819db01f110
Bestfast <31173611+Bestfast@users.noreply.github.com>
2018-03-05 23:47:06 +0100

    FORSE è questa

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -y mingw-w64 libc6-dev-i386
-sudo curl http://www.sqlite.org/2018/sqlite-amalgamation-3220000.zip --output /usr/lib/gcc/x86_64-w64-mingw32/*/include/sqlite.zip
-sudo unzip /usr/lib/gcc/x86_64-w64-mingw32/*/include/sqlite.zip /usr/lib/gcc/x86_64-w64-mingw32/7.2.0/include/
+sudo curl http://www.sqlite.org/2018/sqlite-amalgamation-3220000.zip --output /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/sqlite.zip
+sudo unzip /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/sqlite.zip /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/
 g++ main.cpp -std=c++11 -lsqlite3 -o Generatore_Schede
 x86_64-w64-mingw32-g++ -std=gnu++11 -lsqlite3 main.cpp -o Generatore_Schede.exe
 mkdir build
