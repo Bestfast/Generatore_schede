423b283c83586731d9ca82ae39431e8574f74966
Bestfast <31173611+Bestfast@users.noreply.github.com>
2018-03-05 22:48:17 +0100

    Ennesimo update del build.sh, #include del cavolo

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y mingw-w64
+sudo apt-get install -y mingw-w64 libc6-dev
 g++ main.cpp -std=c++11 -lsqlite3 -o Generatore_Schede
 x86_64-w64-mingw32-g++ -I /usr/include -std=c++11 -lsqlite3 main.cpp -o Generatore_Schede.exe
 mkdir build
