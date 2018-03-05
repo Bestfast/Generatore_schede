b55cc24f4676bad6498e088caccae829c139d74c
Bestfast <31173611+Bestfast@users.noreply.github.com>
2018-03-05 23:07:35 +0100

    mingw ha un po' rotto

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -y mingw-w64 libc6-dev-i386
 g++ main.cpp -std=c++11 -lsqlite3 -o Generatore_Schede
-x86_64-w64-mingw32-g++ -I /usr/include -std=c++11 -lsqlite3 main.cpp -o Generatore_Schede.exe
+x86_64-w64-mingw32-g++ -I /usr/include -std=gnu++11 -lsqlite3 main.cpp -o Generatore_Schede.exe
 mkdir build
 mv Generatore_Schede Generatore_Schede.exe db.sqlite build/
 zip -r build.zip build
