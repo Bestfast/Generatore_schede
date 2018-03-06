65c13f4bb61506ee7f2c20abb171ceb7b70a8da7
Bestfast <31173611+Bestfast@users.noreply.github.com>
2018-03-06 01:52:28 +0100

    Update build.sh
    TECNICAMENTE dovrebbe risolvere l'erroer provocato da ld

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ sudo mv /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/sqlite-amalgamation-3220000/
 sudo mkdir $PWD/sqlite3
 sudo cp /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/sqlite3* $PWD/sqlite3/
 sudo cp /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/shell.c $PWD/sqlite3/
+sudo cp -av /usr/lib/x86_64-linux-gnu/libsqlite3.so.0 /usr/lib/libsqlite3.so.0
+sudo cp -av /usr/lib/x86_64-linux-gnu/libsqlite3.so.0.8.6 /usr/lib/libsqlite3.so.0.8.6
+sudo cp -av /usr/lib/x86_64-linux-gnu/libsqlite3.so.0.8.6 /usr/lib/libsqlite3.so
 g++ main.cpp -std=c++11 -lsqlite3 -o Generatore_Schede
 sed -i 's/<sqlite3.h>/"sqlite3.h"/g' main.cpp
 sudo x86_64-w64-mingw32-g++ -std=gnu++11 main.cpp -lsqlite3 -o Generatore_Schede.exe 
