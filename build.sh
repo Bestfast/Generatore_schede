5a603c4b4eec64ddcf16edb575a3e1a36f349ebb
Bestfast <31173611+Bestfast@users.noreply.github.com>
2018-03-06 02:59:36 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 sudo apt-get install -y gcc-mingw-w64 libc6-dev-i386
 sudo apt-get install --reinstall -y libsqlite3-0:i386
 sudo curl http://www.sqlite.org/2018/sqlite-amalgamation-3220000.zip --output /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/sqlite.zip
+sudo curl https://www.sqlite.org/2018/sqlite-dll-win64-x64-3220000.zip --output /usr/x86_64-w64-mingw32/lib/sqlite_dll.zip
+sudo unzip /usr/x86_64-w64-mingw32/lib/sqlite_dll.zip -d /usr/x86_64-w64-mingw32/lib/
 sudo unzip /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/sqlite.zip -d /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/
 sudo mv /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/sqlite-amalgamation-3220000/* /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/
 sudo mkdir $PWD/sqlite3
@@ -11,7 +13,7 @@ sudo cp /usr/lib/x86_64-linux-gnu/libsqlite3.so.0 /usr/x86_64-w64-mingw32/lib/li
 sudo cp /usr/lib/x86_64-linux-gnu/libsqlite3.so.0.8.6 /usr/x86_64-w64-mingw32/lib/libsqlite3.so.0.8.6
 sudo cp /usr/lib/x86_64-linux-gnu/libsqlite3.so.0.8.6 /usr/x86_64-w64-mingw32/lib/libsqlite3.so
 g++ main.cpp -std=c++11 -lsqlite3 -o Generatore_Schede
-sudo x86_64-w64-mingw32-g++ -std=gnu++11 main.cpp -L/usr/x86_64-w64-mingw32/lib -l:/usr/x86_64-w64-mingw32/lib/libsqlite3.so -o Generatore_Schede.exe 
+sudo x86_64-w64-mingw32-g++ -std=gnu++11 main.cpp -L/usr/x86_64-w64-mingw32/lib -l:/usr/x86_64-w64-mingw32/lib/sqlite3.dll -o Generatore_Schede.exe 
 mkdir build
 sudo mv Generatore_Schede Generatore_Schede.exe db.sqlite build/
 sudo zip -r build.zip build
