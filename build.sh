9f39fff400accdb6235defd2eb757f6dda441242
Bestfast <31173611+Bestfast@users.noreply.github.com>
2018-03-06 16:50:45 +0100

    Update build.sh
    PS: Funziona aggiungendo il dll

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ sudo unzip /usr/i686-w64-mingw32/lib/sqlite_dll.zip -d /usr/i686-w64-mingw32/lib
 sudo unzip /usr/lib/gcc/i686-w64-mingw32/4.8/include/sqlite.zip -d /usr/lib/gcc/i686-w64-mingw32/4.8/include/
 sudo mv /usr/lib/gcc/i686-w64-mingw32/4.8/include/sqlite-amalgamation-3220000/* /usr/lib/gcc/i686-w64-mingw32/4.8/include/
 g++ main.cpp -std=c++11 -lsqlite3 -o Generatore_Schede_linuxX64
-sudo i686-w64-mingw32-g++ -std=gnu++11 main.cpp -static-libgcc -static-libstdc++ -L/usr/i686-w64-mingw32/lib -l:/usr/i686-w64-mingw32/lib/sqlite3.dll  -o Generatore_Schede_winX32.exe 
+sudo i686-w64-mingw32-g++ -std=gnu++11 main.cpp -static-libgcc  -static -lpthread -static-libstdc++ -L/usr/i686-w64-mingw32/lib -static -l:/usr/i686-w64-mingw32/lib/sqlite3.dll  -o Generatore_Schede_winX32.exe 
 mkdir -p build/win32
 mkdir -p build/linux64
 sudo mv Generatore_Schede_linuxX64 db.sqlite build/linux64/
 sudo cp /usr/i686-w64-mingw32/lib/sqlite3.dll $PWD/build/win32/sqlite3.dll
-sudo mv Generatore_Schede_winX32.exe build/win32/Generatore_Schede_winX64.exe
+sudo mv Generatore_Schede_winX32.exe build/win32/Generatore_Schede_winX32.exe
 sudo zip -r build.zip build
