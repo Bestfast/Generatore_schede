3553188e9d8aead3c5652036806d2c0baff181de
Bestfast <31173611+Bestfast@users.noreply.github.com>
2018-03-06 16:36:31 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 sudo apt-get install -y gcc-mingw-w64
-sudo curl http://www.sqlite.org/2018/sqlite-amalgamation-3220000.zip --output /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/sqlite.zip
-sudo curl https://www.sqlite.org/2018/sqlite-dll-win64-x64-3220000.zip --output /usr/x86_64-w64-mingw32/lib/sqlite_dll.zip
-sudo unzip /usr/x86_64-w64-mingw32/lib/sqlite_dll.zip -d /usr/x86_64-w64-mingw32/lib/
-sudo unzip /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/sqlite.zip -d /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/
-sudo mv /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/sqlite-amalgamation-3220000/* /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/
+sudo curl http://www.sqlite.org/2018/sqlite-amalgamation-3220000.zip --output /usr/lib/gcc/i686-w64-mingw32/4.8/include/sqlite.zip
+sudo curl https://www.sqlite.org/2018/sqlite-dll-win32-x86-3220000.zip --output /usr/i686-w64-mingw32/lib/sqlite_dll.zip
+sudo unzip /usr/i686-w64-mingw32/lib/sqlite_dll.zip -d /usr/i686-w64-mingw32/lib/
+sudo unzip /usr/lib/gcc/i686-w64-mingw32/4.8/include/sqlite.zip -d /usr/lib/gcc/i686-w64-mingw32/4.8/include/
+sudo mv /usr/lib/gcc/i686-w64-mingw32/4.8/include/sqlite-amalgamation-3220000/* /usr/lib/gcc/i686-w64-mingw32/4.8/include/
 g++ main.cpp -std=c++11 -lsqlite3 -o Generatore_Schede_linuxX64
-sudo x86_64-w64-mingw32-g++ -std=gnu++11 main.cpp -static-libgcc -static-libstdc++ -L/usr/x86_64-w64-mingw32/lib -l:/usr/x86_64-w64-mingw32/lib/sqlite3.dll  -o Generatore_Schede_winX64.exe 
-mkdir -p build/win64
-sudo mv Generatore_Schede_linuxX64 db.sqlite build/
-sudo cp /usr/x86_64-w64-mingw32/lib/sqlite3.dll $PWD/build/win64/sqlite3.dll
-sudo mv Generatore_Schede_winX64.exe build/win64/Generatore_Schede_winX64.exe
+sudo i686-w64-mingw32-g++ -std=gnu++11 main.cpp -static-libgcc -static-libstdc++ -L/usr/i686-w64-mingw32/lib -l:/usr/i686-w64-mingw32/lib/sqlite3.dll  -o Generatore_Schede_winX32.exe 
+mkdir -p build/win32
+mkdir -p build/linux64
+sudo mv Generatore_Schede_linuxX64 db.sqlite build/linux64/
+sudo cp /usr/i686-w64-mingw32/lib/sqlite3.dll $PWD/build/win32/sqlite3.dll
+sudo mv Generatore_Schede_winX32.exe build/win32/Generatore_Schede_winX64.exe
 sudo zip -r build.zip build
