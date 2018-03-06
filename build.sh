1c796ed0d1988e92ab445821921e765b2dd2ae94
Bestfast <31173611+Bestfast@users.noreply.github.com>
2018-03-06 02:27:18 +0100

    Update build.sh
    Togliamo il symlink, magari è quello

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ sudo mkdir $PWD/sqlite3
 sudo mkdir /usr/lib/gcc/x86_64-w64-mingw32/4.8/lib
 sudo cp /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/sqlite3* $PWD/sqlite3/
 sudo cp /usr/lib/gcc/x86_64-w64-mingw32/4.8/include/shell.c $PWD/sqlite3/
-sudo cp -av /usr/lib/x86_64-linux-gnu/libsqlite3.so.0 /usr/x86_64-w64-mingw32/lib/libsqlite3.so.0
-sudo cp -av /usr/lib/x86_64-linux-gnu/libsqlite3.so.0.8.6 /usr/x86_64-w64-mingw32/lib/libsqlite3.so.0.8.6
-sudo cp -av /usr/lib/x86_64-linux-gnu/libsqlite3.so.0.8.6 /usr/x86_64-w64-mingw32/lib/libsqlite3.so
+sudo cp /usr/lib/x86_64-linux-gnu/libsqlite3.so.0 /usr/x86_64-w64-mingw32/lib/libsqlite3.so.0
+sudo cp /usr/lib/x86_64-linux-gnu/libsqlite3.so.0.8.6 /usr/x86_64-w64-mingw32/lib/libsqlite3.so.0.8.6
+sudo cp /usr/lib/x86_64-linux-gnu/libsqlite3.so.0.8.6 /usr/x86_64-w64-mingw32/lib/libsqlite3.so
 g++ main.cpp -std=c++11 -lsqlite3 -o Generatore_Schede
 sed -i 's/<sqlite3.h>/"sqlite3.h"/g' main.cpp
 sudo x86_64-w64-mingw32-g++ -std=gnu++11 main.cpp -L/usr/x86_64-w64-mingw32/lib -lsqlite3 -o Generatore_Schede.exe 
